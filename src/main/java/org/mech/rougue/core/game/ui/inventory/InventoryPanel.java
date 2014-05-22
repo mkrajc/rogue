1dc0b3e5e11158d0babf9610a4e03bcf87018b87
mkrajc <krajc.martin@gmail.com>
2014-05-22 14:50:47 +0200

    total weight label

--- src/main/java/org/mech/rougue/core/game/ui/inventory/InventoryPanel.java
+++ src/main/java/org/mech/rougue/core/game/ui/inventory/InventoryPanel.java
@@ -9,6 +9,8 @@ import java.util.List;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
 import org.mech.rougue.core.game.GameContext;
 import org.mech.rougue.core.game.ui.ShowPanel;
@@ -21,6 +23,7 @@ import org.mech.rougue.lang.LocalizedResourceBundle;
 public class InventoryPanel extends ShowPanel {
 
 	private static final long serialVersionUID = 6726987122781268642L;
+	private static final String TOTAL_WEIGHT_KEY = "inv.total.weight";
 
 	@Inject
 	private LocalizedResourceBundle bundle;
@@ -31,17 +34,24 @@ public class InventoryPanel extends ShowPanel {
 	private JTable inventoryTable;
 	private Inventory inventory;
 	private AbstractTableModel inventoryTableModel;
+	private JLabel totalWeightLabel;
 
 	public InventoryPanel() {
 		addFocusListener(this.new Focus());
 	}
-
 
 	public void setInventory(final Inventory inventory) {
 		removeAll();
 
 		this.inventory = inventory;
 		this.inventoryTableModel = new AllItemsTableModel(inventory, bundle);
+		this.inventoryTableModel.addTableModelListener(new TableModelListener() {
+			@Override
+			public void tableChanged(final TableModelEvent e) {
+				totalWeightLabel.setText(createTotalWeightLabel());
+			}
+		});
+		this.totalWeightLabel = new JLabel(createTotalWeightLabel());
 		this.inventoryTable = new JTable(inventoryTableModel);
 
 		inventoryTable.setFillsViewportHeight(true);
@@ -50,9 +60,13 @@ public class InventoryPanel extends ShowPanel {
 		inventoryTable.addKeyListener(this.new DropItemKeyListener());
 
 		final JScrollPane scrollPane = new JScrollPane(inventoryTable);
-		add(scrollPane);
-		add(new JLabel("Total: " + inventory.getTotalWeight()));
+		add(scrollPane, "wrap");
+		add(totalWeightLabel, "wrap");
 
+	}
+
+	private String createTotalWeightLabel() {
+		return bundle.getMessage(TOTAL_WEIGHT_KEY, inventory.getTotalWeight());
 	}
 
 	protected void dropSelected() {
